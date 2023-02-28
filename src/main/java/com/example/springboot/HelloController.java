046b66ada08f0496a8a4f24e8403e08bb8024b8b
Mohamed Shahat <mshahat@vmware.com>
2023-02-28 01:07:23 +0000

    update the message with html

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "<h1>Hello AJ Bell, Greetings from Spring Boot + Tanzu!</h1>";
 	}
 
 }
